d2acf65778e0a39283a13c8f8e1f475ddfdaff49
Zhongheng Yang <32523793+michaelyangbling@users.noreply.github.com>
2018-04-11 22:31:17 -0400

    Update nn.py

--- nn.py
+++ nn.py
@@ -15,10 +15,13 @@ scaledTst=scaler.transform(data["X_test"])
 scaledValid=scaler.transform(data["X_validation"])
 def sigmoid(x):
     return 1/(1+math.exp(-x))
-
-def nn(input,label,actiFunc,layer2num,layer3num): #nn for binary classification
+def derivSigmoid(x):
+    return sigmoid(x) * ( 1-sigmoid(x) )
+def nn(input,labels,actiFunc, deriv, layer2num,layer3num): #nn for binary classification
     inputDim=input.shape[1]
+    num=input.shape[0]
     vAct = np.vectorize(actiFunc)
+    vDerivAct=np.vectorize(deriv)
     weight=[]
     weight.append(np.random.normal(0,0.01,(layer2num,inputDim)))
     weight.append(np.random.normal(0, 0.01, (layer3num, layer2num)))
@@ -28,8 +31,8 @@ def nn(input,label,actiFunc,layer2num,layer3num): #nn for binary classification
     bias.append(np.random.normal(0, 0.01, (layer3num, 1)))
     bias.append(np.random.normal(0, 0.01, (1, 1)))
     while True:
-        aAll=[];zAll=[]
-        for i in range(0,input.shape[0]):
+        aAll=[];zAll=[] #forward prop
+        for i in range(0,num):
           a=[];z=[]
           a.append(np.reshape(input[i,:],(2,1)))
           z.append(np.reshape(input[i,:],(2,1)))
@@ -38,6 +41,23 @@ def nn(input,label,actiFunc,layer2num,layer3num): #nn for binary classification
             a.append(vAct(z[j]))
           aAll.append(a)
           zAll.append(z)
+            
+        dWeight=[]
+        dWeight=dWeight.append(np.zeros((layer2num,inputDim)))
+        dWeight=dWeight.append(np.zeros((layer3num, layer2num)))
+        dweight=dWeight.append(np.zeros((1,layer3num)))
+        
+        for k in range(0,num): #backward prop #begin with zero in code,but with 1 in math
+          delta=list(range(0,4))
+          y=float(labels[k,:])
+          zLast=float(zAll[k][3])
+          delta[3]=np.array([[ ( y/actiFunc(zLast) + (y-1)/actiFunc(zLast) ) * deriv( zLast ) ]] )
+          delta[2]=np.multiply( np.matmul( weight[k][2].transpose(), delta[3] ), vDerivAct( zAll[k][2] ) )
+          delta[1]=np.multiply( np.matmul( weight[k][1].transpose(), delta[2] ), vDerivAct( zAll[k][1] ) )
+          for l in range(0,3):
+            dWeight[l]=d
+          
+        
 
 
 
